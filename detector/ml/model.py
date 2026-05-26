da9d82f95556a23a210a6374726e23a86077c0c3
Rucksolly <rucksolly.tech@gmail.com>
2026-05-26 11:50:51 +0200

    Added global model

--- detector/ml/model.py
+++ detector/ml/model.py
@@ -7,6 +7,7 @@ import json
 from django.conf import settings
 from huggingface_hub import hf_hub_download
 import os
+torch.set_num_threads(1)
 
 IMG_SIZE = 384
 
@@ -35,41 +36,54 @@ def _build_model(num_classes=196):
 _model = None
 _class_names = None
 
-# def get_model():
-#     global _model, _class_names
-#     if _model is None:
-#         checkpoint = torch.load(settings.ML_MODEL_PATH, map_location='cpu',weights_only=False)
-#         _class_names = checkpoint['class_names']
-#         _model = _build_model()
-#         _model.load_state_dict(checkpoint['model_state_dict'])
-#         _model.eval()
-#     return _model, _class_names
+torch.set_num_threads(1)
 
 def get_model():
     global _model, _class_names
+
     if _model is None:
-        # model_path = settings.ML_MODEL_PATH
-        # if not os.path.exists(model_path):
         model_path = hf_hub_download(
             repo_id=settings.HF_REPO_ID,
-            filename="cardetect_final.pt"
+            filename="cardetect_final.pt",
+            cache_dir="/tmp/hf_cache"
         )
-        checkpoint = torch.load(model_path, map_location='cpu',weights_only=False)
-        _class_names = checkpoint['class_names']
-        _model = _build_model()
-        _model.load_state_dict(checkpoint['model_state_dict'])
-        _model.eval()
+
+        checkpoint = torch.load(
+            model_path,
+            map_location="cpu",
+            weights_only=False
+        )
+
+        _class_names = checkpoint["class_names"]
+
+        model = _build_model()
+        model.load_state_dict(checkpoint["model_state_dict"])
+        model.eval()
+
+        _model = model
+
     return _model, _class_names
+
 
 def predict(image_file, top_k=5):
     model, class_names = get_model()
-    img = Image.open(image_file).convert('RGB')
+
+    img = Image.open(image_file).convert("RGB")
     tensor = val_transform(img).unsqueeze(0)
-    with torch.no_grad():
+
+    with torch.inference_mode():
         logits = model(tensor)
+
     probs = torch.softmax(logits, dim=1)
     top = torch.topk(probs, top_k)
+
     return [
-        {'class': class_names[i], 'confidence': round(p * 100, 2)}
-        for i, p in zip(top.indices[0].tolist(), top.values[0].tolist())
+        {
+            "class": class_names[i],
+            "confidence": round(p * 100, 2),
+        }
+        for i, p in zip(
+            top.indices[0].tolist(),
+            top.values[0].tolist()
+        )
     ]
